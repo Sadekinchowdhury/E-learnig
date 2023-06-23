5720b81ca04ad55b7aa67ba5281b5199c2f87414
dev-sweet <sweetali0520@gmail.com>
2023-06-24 04:22:45 +0600

    private and public notices are added

--- src/components/notices/PrivateNotice.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function PrivateNotice() {
-  return (
-    <div>PrivateNotice</div>
-  )
-}
--- src/components/notices/PublicNotice.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function PublicNotice() {
-  return (
-    <div>PublicNotice</div>
-  )
-}
