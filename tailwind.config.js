5633f30e5730480b9aeaee782e764fe65a5af054
showkatbinidris70 <showkatbinidris70@gmail.com>
2023-06-25 23:10:16 +0600

    home page banner added

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,8 @@ module.exports = {
                 'hero': "url('assects/images/marketing-bg6.png')",
                 'appDevelopment': "url('assects/images/app-development.jpg')",
                 'certificateImage': "url('assects/images/watermark.jpg')",
+                'homeBanner': "url('assects/images/bg-image.png')",
+                'homeBanner2': "url('assects/images/meeting.jpg')",
                 'drowing': "url('assects/images/drowing.PNG')",
                 'app-learing': "url('dist/iamges/marketing-bg6.png')"
             }
