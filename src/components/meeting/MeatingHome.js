ab1d6b0ec9af4d6d9401657546b37e71c76c79e9
dev-sweet <sweetali0520@gmail.com>
2023-07-06 18:08:02 -0300

    meeting slider section added in homepage

--- src/components/meeting/MeatingHome.js
+++ src/components/meeting/MeatingHome.js
@@ -43,47 +43,50 @@ const MeatingHome = () => {
   return (
     <section className="bg-[#091650] py-12 lg:px-[200px] px-4 md:px-8">
       <div className="text-center">
-        <h3 className="text-xl text-[#0ad3ef]">WHAT WE OFFER</h3>
-        <h1 className="text-4xl font-bold text-white mt-3">
+        <h1 className="text-4xl font-bold text-white">
           Our Flexible Pricing Plans
         </h1>
       </div>
       <div className="mt-8">
         <Slider {...settings}>
           {pricing.map((item, i) => (
-            <div key={i * 10} className="pricing-card bg-[#16256e] py-8">
-              <div className="flex flex-col content-center items-center">
-                <img className="w-[100px]" src={item.img} />
-                <h3 className="text-2xl text-white mt-4 mb-4">{item.title}</h3>
-                <h2 className="text-5xl font-bold text-[#0ad3ef]">
-                  ${item.price}
-                </h2>
-                <span className="text-lg font-normal text-gray-400 mt-2">
-                  / Per Month
-                </span>
-              </div>
-              <ul className="list-none mt-4 px-7">
-                {item.infos.map((info, i) => (
-                  <li
-                    key={i + 7}
-                    className={`flex mb-2 gap-2 items-center text-gray-400 text-lg ${
-                      i == 0 && "text-bold"
-                    }`}
-                  >
-                    <span className="text-[#0ad3ef]">
-                      <BsFillCheckCircleFill />
-                    </span>
-                    {info}
-                  </li>
-                ))}
-              </ul>
+            <div key={i * 3}>
+              <div className="pricing-card bg-[#16256e] py-8 mx-3">
+                <div className="flex flex-col content-center items-center">
+                  <img className="w-[100px]" src={item.img} />
+                  <h3 className="text-2xl text-white mt-4 mb-4">
+                    {item.title}
+                  </h3>
+                  <h2 className="text-5xl font-bold text-[#0ad3ef]">
+                    ${item.price}
+                  </h2>
+                  <span className="text-lg font-normal text-gray-400 mt-2">
+                    / Per Month
+                  </span>
+                </div>
+                <ul className="list-none mt-4 px-7">
+                  {item.infos.map((info, i) => (
+                    <li
+                      key={i + 7}
+                      className={`flex mb-2 gap-2 items-center text-gray-400 text-lg ${
+                        i == 0 && "text-bold"
+                      }`}
+                    >
+                      <span className="text-[#0ad3ef]">
+                        <BsFillCheckCircleFill />
+                      </span>
+                      {info}
+                    </li>
+                  ))}
+                </ul>
 
-              <div className="pt-4 text-center">
-                <Link className="mx-auto" to="/payment">
-                  <button className="flex mx-auto items-center content-center btn btn-lg h-[50px] px-8 py-4 text-white text-xl bg-[#0ad3ef] rounded-[50px] border border-[#0ad3ef] hover:bg-[#091650]">
-                    Get Started
-                  </button>
-                </Link>
+                <div className="pt-4 text-center">
+                  <Link className="mx-auto" to="/payment">
+                    <button className="flex mx-auto items-center content-center btn btn-lg h-[50px] px-8 py-4 text-white text-xl bg-[#0ad3ef] rounded-[50px] border border-[#0ad3ef] hover:bg-[#091650]">
+                      Get Started
+                    </button>
+                  </Link>
+                </div>
               </div>
             </div>
           ))}
