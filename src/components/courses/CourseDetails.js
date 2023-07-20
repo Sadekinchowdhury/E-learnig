7410be8a3a05b34a2e3d95ac086782d27bb3a151
dev-sweet <sweetali0520@gmail.com>
2023-07-20 16:23:43 +0600

    course details completed

--- src/components/courses/CourseDetails.js
+++ src/components/courses/CourseDetails.js
@@ -1,13 +1,42 @@
 import React from "react";
 import { BsCalendarDate } from "react-icons/bs";
-import { BsFillCalendar2DateFill } from "react-icons/bs";
 import { BsClockFill } from "react-icons/bs";
 import { FaShoppingCart } from "react-icons/fa";
 import { FaCheck } from "react-icons/fa";
-import { BiDesktop } from "react-icons/bi";
 import { AiTwotonePlusSquare } from "react-icons/ai";
-import mentor from "../../assects/images/mentor_details_image_1.jpg";
+import { FaInfo } from "react-icons/fa";
+import { BsFillPersonFill } from "react-icons/bs";
+import { BsFillBookmarksFill } from "react-icons/bs";
+import { BsFillSunFill } from "react-icons/bs";
+import { BsGift } from "react-icons/bs";
+import { GiBlackBook } from "react-icons/gi";
 const CourseDetails = () => {
+  const earnings = [
+    {
+      days: "Monday",
+      time: "02:30 - 4:30",
+      date: "22/07/2023",
+      earnings: "Nonstop",
+    },
+    {
+      days: "Saturday",
+      time: "02:30 - 4:30",
+      date: "22/07/2023",
+      earnings: "Nonstop",
+    },
+    {
+      days: "Wednesday",
+      time: "02:30 - 4:30",
+      date: "22/07/2023",
+      earnings: "Nonstop",
+    },
+    {
+      days: "Thirsday",
+      time: "02:30 - 4:30",
+      date: "22/07/2023",
+      earnings: "Nonstop",
+    },
+  ];
   return (
     <div>
       <section className="bg-[#f6f6f6] lg:py-16 py-6">
@@ -52,7 +81,7 @@ const CourseDetails = () => {
 
       <section className="lg:py-16 md:py-12 py-6">
         <div className="grid lg:grid-cols-2 gap-10  w-4/5 mx-auto">
-          <div>
+          <div className="h-full flex flex-col justify-center">
             <h2 className="lg:text-5xl md:text-5xl sm:text-4xl text-3xl font-bold">
               What You will Learn
             </h2>
@@ -174,6 +203,193 @@ const CourseDetails = () => {
               </ul>
             </div>
           </div>
+        </div>
+      </section>
+      <section className="lg:py-16 md:py-12 py-6">
+        <div className="w-4/5 mx-auto">
+          <div>
+            <h2 className="text-uppercase text-3xl mb-5 font-semibold">
+              Expert web design and Development live course
+            </h2>
+            <p className="text-lg">
+              This live course is specifically tailored for individuals who
+              already possess a foundational understanding of web design and
+              development and are looking to elevate their expertise to an
+              advanced level. It covers a wide range of topics related to both
+              web design and development. The web design aspect of the course
+              focuses on advanced principles and techniques, including user
+              experience (UX) design, user interface (UI) design, visual design,
+              and responsive design. Participants will learn how to create
+              engaging and user-friendly website interfaces, incorporating
+              elements such as typography, color theory, and layout composition.
+            </p>
+          </div>
+          <div className="pt-5">
+            <div className="flex items-center">
+              <FaInfo className="text-sky-600" size={26} />
+              <h3 className="text-5xl ml-2 font-semibold">Description</h3>
+            </div>
+            <p className="px-9 pt-5 text-lg">
+              The "Expert Web Design and Development Live Course" is an
+              intensive program that advances participants' skills in web design
+              and development. It covers advanced topics in UX/UI design,
+              responsive design, front-end and back-end development, and offers
+              live instruction, fostering interactive learning for enhanced
+              expertise.It covers a wide range of topics related to both web
+              design and development. The web design aspect of the course
+              focuses on advanced principles and techniques, including user
+              experience (UX) design, user interface (UI) design, visual design,
+              and responsive design.
+            </p>
+          </div>
+          <div className="pt-5">
+            <div className="flex items-center">
+              <BsFillPersonFill className="text-sky-600" size={23} />
+              <h3 className="text-3xl ml-2 font-semibold">
+                Who is the course for?
+              </h3>
+            </div>
+            <p className="px-9 pt-5 text-lg">
+              The course for web development is typically designed for
+              individuals who want to learn how to create websites and web
+              applications. It is suitable for beginners with little or no prior
+              programming experience, as well as for those with some programming
+              knowledge who want to expand their skills in web development. The
+              "Expert Web Design and Development Live Course" is an intensive
+              program that advances participants' skills in web design and
+              development. It covers advanced topics in UX/UI design, responsive
+              design, front-end and back-end development, and offers live
+              instruction, fostering interactive learning for enhanced
+              expertise.
+            </p>
+          </div>
+          <div className="pt-5">
+            <div className="flex items-center">
+              <GiBlackBook className="text-sky-600" size={23} />
+              <h3 className="text-3xl ml-2 font-semibold">
+                What will be taught in the course
+              </h3>
+            </div>
+            <p className="px-9 pt-5 text-lg">
+              The specific content of a web development course can vary
+              depending on the curriculum and the level of the course (beginner,
+              intermediate, or advanced). However, in a comprehensive web
+              development course, you can expect to learn the following topics:
+              The web design aspect of the course focuses on advanced principles
+              and techniques, including user experience (UX) design, user
+              interface (UI) design, visual design, and responsive design.
+            </p>
+          </div>
+          <div className="pt-5">
+            <div className="flex items-center">
+              <BsFillBookmarksFill className="text-sky-600" size={23} />
+              <h3 className="text-3xl ml-2 font-semibold">
+                What is required before starting the course?
+              </h3>
+            </div>
+            <p className="px-9 pt-5 text-lg">
+              The "Expert Web Design and Development Live Course" is an
+              intensive program that advances participants' skills in web design
+              and development. It covers advanced topics in UX/UI design,
+              responsive design, front-end and back-end development, and offers
+              live instruction, fostering interactive learning for enhanced
+              expertise.It covers a wide range of topics related to both web
+              design and development. The web design aspect of the course
+              focuses on advanced principles and techniques, including user
+              experience (UX) design, user interface (UI) design, visual design,
+              and responsive design.
+            </p>
+          </div>
+          <div className="pt-5">
+            <div className="flex items-center">
+              <BsFillSunFill className="text-sky-600" size={23} />
+              <h3 className="text-3xl ml-2 font-semibold">
+                With special benefits
+              </h3>
+            </div>
+            <p className="px-9 pt-5 text-lg">
+              The "Expert Web Design and Development Live Course" is an
+              intensive program that advances participants' skills in web design
+              and development. It covers advanced topics in UX/UI design,
+              responsive design, front-end and back-end development, and offers
+              live instruction, fostering interactive learning for enhanced
+              expertise.It covers a wide range of topics related to both web
+              design and development. The web design aspect of the course
+              focuses on advanced principles and techniques, including user
+              experience (UX) design, user interface (UI) design, visual design,
+              and responsive design.
+            </p>
+          </div>
+          <div className="pt-5">
+            <div className="flex items-center">
+              <BsGift className="text-sky-600" size={23} />
+              <h3 className="text-3xl ml-2 font-semibold">
+                Areas of earnings at the end of the course:
+              </h3>
+            </div>
+            <p className="px-9 pt-5 text-lg">
+              The "Expert Web Design and Development Live Course" is an
+              intensive program that advances participants' skills in web design
+              and development. It covers advanced topics in UX/UI design,
+              responsive design, front-end and back-end development, and offers
+              live instruction, fostering interactive learning for enhanced
+              expertise.It covers a wide range of topics related to both web
+              design and development. The web design aspect of the course
+              focuses on advanced principles and techniques, including user
+              experience (UX) design, user interface (UI) design, visual design,
+              and responsive design.
+            </p>
+          </div>
+
+          <table className="min-w-full mt-5 divide-y divide-gray-200 rounded-2xl">
+            <thead className="">
+              <tr className="">
+                <th
+                  scope="col"
+                  className="bg-[#ff1919] text-center px-4 py-4 font-semibold text-white uppercase tracking-wider"
+                >
+                  Days
+                </th>
+                <th
+                  scope="col"
+                  className="bg-[#ff1919] text-center px-4 py-4 font-semibold text-white uppercase tracking-wider"
+                >
+                  Time
+                </th>
+                <th
+                  scope="col"
+                  className="bg-[#ff1919] text-center px-4 py-4 font-semibold text-white uppercase tracking-wider"
+                >
+                  Date
+                </th>
+
+                <th
+                  scope="col"
+                  className="bg-[#ff1919] text-center px-4 py-34text-xs font-semibold text-white uppercase tracking-wider"
+                >
+                  Earnings
+                </th>
+              </tr>
+            </thead>
+            <tbody className="divide-y divide-gray-200">
+              {earnings.map((e, i) => (
+                <tr key={i}>
+                  <th className="border-1  bg-gray-100 px-6 py-3 text-xsm text-black whitespace-nowrap">
+                    {e.days}
+                  </th>
+                  <td className="border-1  bg-gray-100 px-6 py-3 text-xsm text-black whitespace-nowrap">
+                    {e.time}
+                  </td>
+                  <td className="border-1  bg-gray-100 px-6 py-3 text-xsm text-black whitespace-nowrap">
+                    {e.date}
+                  </td>
+                  <td className="border-1  bg-gray-100 px-6 py-3 text-xsm text-black whitespace-nowrap">
+                    {e.earnings}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </section>
     </div>
